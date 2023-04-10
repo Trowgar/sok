bcc03f8741f1c0a232ccd5b2d18c9e2fbfb5e611
Trowgar <trowgar@yahoo.com>
2023-04-11 02:34:42 +0300

    Small Updates

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ bot.on('message', async (msg) => {
     const text = msg.text;
 
     if(text === '/start') {
-        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+        await bot.sendMessage(chatId, 'Please fill the form bellow', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: clientUrl + '/form'}}]
+                    [{text: 'Fill the form', web_app: {url: clientUrl + '/form'}}]
                 ]
             }
         })
 
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+        await bot.sendMessage(chatId, 'Click the button to visit our store', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: clientUrl}}]
+                    [{text: 'Make Order', web_app: {url: clientUrl}}]
                 ]
             }
         })
@@ -37,12 +37,12 @@ bot.on('message', async (msg) => {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
             console.log(data)
-            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
-            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+            await bot.sendMessage(chatId, 'Thank you!')
+            await bot.sendMessage(chatId, 'You Country: ' + data?.country);
+            await bot.sendMessage(chatId, 'Your Address: ' + data?.street);
 
             setTimeout(async () => {
-                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+                await bot.sendMessage(chatId, 'All Information you will get in this chat');
             }, 3000)
         } catch (e) {
             console.log(e);
@@ -56,9 +56,9 @@ app.post('/web-data', async (req, res) => {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
-            title: 'Успешная покупка',
+            title: 'Successful Purchase',
             input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+                message_text: ` Thank you for order, price is ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
         })
         return res.status(200).json({});
